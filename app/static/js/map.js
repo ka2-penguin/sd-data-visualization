ebc55e93e83fbb14b8fb99952487c09665b9520d
dyredhead <danielyentin@gmail.com>
2023-05-12 08:48:56 -0400

    qc

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -6,22 +6,6 @@
     // Use the 'v' parameter to indicate the version to load (alpha, beta, weekly, etc.)
   });  
 
-var getJSON = function(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status === 200) {
-      callback(null, xhr.response);
-    } else {
-      callback(status, xhr.response);
-    }
-  };
-  xhr.send();
-};
-
-
 let map;
 async function initMap() {
   const obj = getJSON("/query.json");
