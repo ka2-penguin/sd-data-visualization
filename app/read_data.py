334b4b3277992af30a3a632e9585aecd2464a90f
dyredhead <danielyentin@gmail.com>
2023-05-15 13:37:13 -0400

    made sql filter

--- app/read_data.py
+++ app/read_data.py
@@ -13,13 +13,40 @@ def db_close():
     db.commit()
     db.close()
 
-def get_trips(trip_duration,year,month,day,hour,minute,is_member,start_station_id,end_station_id):
+def get_trips(filters):
     c = db_connect()
-    c.execute('SELECT * FROM TRIPS WHERE year=?,month=?, by year SORT BY DEC', ())
+    filter = create_sql_filter(filters)
+    c.execute(filter, ())
     db_close()
 
-def create_filter(filters):
-    filter = "SELECT * FROM TRIPS WHERE"
+def create_sql_filter(filters: dict[str, str]) -> str:
+    filter = "SELECT * FROM TRIPS"
+    # will not work becuase the dict still has stuff inside, even if they are empty strings
+    # possible solution is a try catch statement that defaults to getting the 100 most recent trips
+    if filters: 
+        filter += " WHERE"
+    if filters["min_trip_duration"]:
+        filter += f", trip_duration >= " + filters["min_trip_duration"]
+    if filters["max_trip_duration"]:
+        filter += f", trip_duration <= " + filters["max_trip_duration"]
+
+    if filter["min_date"]:
+        date = filter["min_date"].split(",")
+        filter += f", year >= {date[0]}, month >= {date[1]}, day >= {date[2]}, hour >= {date[3]}, min >= {date[4]}"
+    if filter["max_date"]:
+        date = filter["min_date"].split(",")
+        filter += f", year <= {date[0]}, month <= {date[1]}, day <= {date[2]}, hour <= {date[3]}, min <= {date[4]}"
+
+    if filter["is_member"]:
+        filter += ", is_member = " + filter["is_member"]
+
+    if filter["start_station_id"]:
+        filter += ", start_station_id = " + filter["start_station_id"]
+    if filter["end_station_id"]:
+        filter += ", end_station_id = " + filter["end_station_id"]
+    
+    filter += ",BY year SORT BY dec"
+    return filter
 
 
 def get_ridership_by_month():
