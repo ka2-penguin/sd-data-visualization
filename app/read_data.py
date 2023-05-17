8c25b93e7a63bd4b13ad7ede24fe69dde6d36b6e
dyredhead <danielyentin@gmail.com>
2023-05-17 09:49:11 -0400

    fixed bugs, sql filter works now, tested

--- app/read_data.py
+++ app/read_data.py
@@ -20,19 +20,12 @@ def get_trips(filters) -> list[list[str]]:
     db_close()
     return results
 
-def is_empty(dict):
-    for i in dict.values():
-        if i != "":
-            return False
-    return True
-
 def create_sql_filter(filters: dict[str, str]) -> str:
     filter = f"SELECT * FROM trips"
     # will not work becuase the dict still has stuff inside, even if they are empty strings
     # possible solution is a try catch statement that defaults to getting the 100 most recent trips
-    is_empty = is_empty(filters)
 
-    if not is_empty: 
+    if not are_filters_empty(filters): 
         filter += " WHERE"
     if filters["min_trip_duration"]:
         filter += f" AND trip_duration >= " + filters["min_trip_duration"]
@@ -63,16 +56,21 @@ def create_sql_filter(filters: dict[str, str]) -> str:
     if filters["end_station_id"]:
         filter += " AND end_station_id = " + filters["end_station_id"]
     
-    if not is_empty:
+    if not are_filters_empty(filters):
         filter += f" ORDER BY year DESC LIMIT {MAX_RESULTS}"
-
-    # remove extra comma at the start of filter
-    i = filter.find("WHERE") + len("WHERE")
-    filter = filter[:i] + filter[i+len("AND")+1:]
+        
+        # remove extra comma at the start of filter
+        i = filter.find("WHERE") + len("WHERE")
+        filter = filter[:i] + filter[i+len("AND")+1:]
 
     print(f"SQL filter: {filter}")
     return filter
 
+def are_filters_empty(filters):
+    for i in filters.values():
+        if i != "":
+            return False
+    return True
 
 def get_ridership_by_month():
     c = db_connect()
